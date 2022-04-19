547ca2240d700a405a05bf2d6b53719864ecc111
web-coderslink <ddeffghhi@gmail.com>
2022-04-19 20:40:41 +0530

    init

---
+++ portfolio-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h4>my first web hosting woth netlify</h4>
+    </div>
+  );
+}
+
+export default App;
